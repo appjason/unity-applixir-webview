0a7d9c829089f05fc2c273238ef17f4d5eb1fec5
Jason Kennaly <jason@applixir.com>
2022-03-30 17:07:09 -0700

    basic button test app

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2012 GREE, Inc.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    private string lastMessage;
+
+    void Start() { }
+
+    public void ButtonClick()
+    {
+        string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+        lastMessage = "Button Clicked " + datetime;
+        
+    }
+
+    void OnGUI()
+    {
+        var x = 10;
+
+        GUIStyle style = new GUIStyle("textField");
+        style.fontSize = 36;
+        GUI.TextField(new Rect(x, 10, 360, 80), "" + lastMessage, 25, style);
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# button project
+
+This project is intended as a simple Unity project to demonstrate AppLixir integrations. This requires a version of Unity of 2020 or newer.
